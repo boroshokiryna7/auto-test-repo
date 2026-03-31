641b8335fb9a0a9f87c275c5859d35f568abc88a
IrynaFal <boroshok.iryna7@gmail.com>
2026-03-31 20:30:08 +0200

    fix for the 6 task

--- jsBasics/homeTasks8.1/task6.js
+++ jsBasics/homeTasks8.1/task6.js
@@ -1,3 +1,7 @@
 const numbersList = [1,10,14,2,4,5,43,34]
-const sortedNumbersList = numbersList.sort((a, b) => a - b);
-console.log(`Sorted numbers: ${sortedNumbersList}`);    
+const copyOfNumbersList = numbersList.slice();
+
+copyOfNumbersList.sort((a, b) => a - b);  
+  
+console.log (`Original array: ${numbersList}`);
+console.log (`Sorted array: ${copyOfNumbersList}`); 
